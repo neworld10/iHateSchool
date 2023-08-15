e3ad365e58d46ef5cfd6e7379decc55760287b12
neworld10 <luffy0630@gmail.com>
2023-08-15 10:38:33 +0900

    Dart Project Create 2023/08/13 TARGET Summer Vacation Practice

---
+++ TARGET/230813/p2/bin/p2.dart
@@ -0,0 +1,6 @@
+// 문제 2 - 두 값을 더한 값을 return하는 add() 함수를 작성하시오
+void main() {
+  int x = 10;
+  int y = 25;
+  print(add(x, y));
+}
---
+++ TARGET/230813/p3/bin/p3.dart
@@ -0,0 +1,3 @@
+//문제 3 - Named Parameter를 이용해서 유저의 Name, ID, Password를 받고 출력하는 함수를 작성하시오.
+
+void main() {}
