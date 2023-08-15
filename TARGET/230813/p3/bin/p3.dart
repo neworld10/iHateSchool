7ce322769bfa81f519705fac29e00dac8dc1eda7
neworld10 <luffy0630@gmail.com>
2023-08-15 11:27:33 +0900

    p3 해결

--- TARGET/230813/p3/bin/p3.dart
+++ TARGET/230813/p3/bin/p3.dart
@@ -1,13 +1,9 @@
 //문제 3 - Named Parameter를 이용해서 유저의 Name, ID, Password를 받고 출력하는 함수를 작성하시오.
-import 'dart:io';
 
-void signIn() {
-  String? name = stdin.readLineSync();
-  String? id = stdin.readLineSync();
-  String? pwd = stdin.readLineSync();
+void signIn(String name, String id, String pwd) {
   print("name: $name / ID: $id / Password: $pwd");
 }
 
 void main() {
-  signIn();
+  signIn('target', 'dart', 'hello');
 }
