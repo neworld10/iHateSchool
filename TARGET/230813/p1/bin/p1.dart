562379340974f233f773694c77596a4b9db863ed
neworld10 <luffy0630@gmail.com>
2023-09-04 14:10:26 +0900

    걍 다 날림 ㅅㄱ

--- TARGET/230813/p1/bin/p1.dart
+++
@@ -1,6 +0,0 @@
-// 문제 1 - 고양이 이름을 Collection for를 통해서 모두 List<String> ans 에 담아 출력하시오
-void main() {
-  var catName = ["Danto", "Cheese", "Lucy"];
-  List<String> ans = [for (var i in catName) '$i'];
-  print(ans);
-}
--- TARGET/230813/p3/bin/p3.dart
+++
@@ -1,9 +0,0 @@
-//문제 3 - Named Parameter를 이용해서 유저의 Name, ID, Password를 받고 출력하는 함수를 작성하시오.
-
-signIn({String? name, String? id, String? pwd}) {
-  print("name: $name / ID: $id / Password: $pwd");
-}
-
-void main() {
-  signIn(pwd: '죽여주마', name: '리쿠하치마', id: '아루');
-}
