2610c92a46d01e6cad7f8b9784fe7551eb16a909
neworld10 <luffy0630@gmail.com>
2023-10-16 14:15:23 +0900

    뭐지?

--- TARGET/230911/discord_copyinh/android/app/src/main/kotlin/com/example/discord_copyinh/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.discord_copyinh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- TARGET/231016/discord_copy/android/app/src/main/kotlin/com/example/discord_copy/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.discord_copy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
